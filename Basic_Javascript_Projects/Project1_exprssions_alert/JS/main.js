535d0ea3773b5e2eccb9a82aebc14f17b868220c
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-08-02 17:19:16 -0400

    javascript
    first submission

---
+++ Basic_Javascript_Projects/Project1_exprssions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Javascript");
+var X = "Hello"; //We assigned var X to the value of "Hello"
+var Y = "World"; //We assigned var Y to the value of "World"
+var green = "Hello"; //Var X will now display with a font color of green
+green = green.fontcolor("green");
+var blue = "World"; //Var Y will now display with a font color of blue
+blue = blue.fontcolor("blue");
+document.write(green + " " +blue + "\n");//We took the fontcolor to display the variable while including a line space for the value of the expression.
+document.write(3 + 3); //Expression
