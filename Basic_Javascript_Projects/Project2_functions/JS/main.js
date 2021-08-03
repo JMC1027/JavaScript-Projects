b1e5a0dcaa889a231175dc92435d1e49a309d119
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-08-03 13:36:44 -0400

    my first function

---
+++ Basic_Javascript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+    var sentence = "Snowboard seaason is coming";
+    sentence += " get your board ready";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
