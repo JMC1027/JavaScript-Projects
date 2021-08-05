d0a6776fd856a3acbc7308bb0756d7c0db210393
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-08-05 15:31:26 -0400

    Update main.js

--- Basic_Javascript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,12 +1,12 @@
 document.write(typeof "Snowboard" + " ");
-document.write(2 + 2 + " ");
+document.write(2 + 2 + " "); //the + is the string and the 2 is the number
 
-document.write(10 > 5 && 12 > 6);
-document.write(15 > 10 || 10 > 15);
-document.write(12 == 12);
+document.write(10 > 5 && 12 > 6); // the && is showing that both are true
+document.write(15 > 10 || 10 > 15); // the || is showing that one OR the other is true
+document.write(12 == 12); // == used to check that the value is true to the expression given, here we are simply provig 12 is equal to 12 which displays as true.
 x = 20;
 z = 20;
-document.write(x === z);
+document.write(x === z); //=== shows that the date on both sides has the same value 
 document.write(25 > 24);
 document.write(30 < 31);
 function not_Function() {
