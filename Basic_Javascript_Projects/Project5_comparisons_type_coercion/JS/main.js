84d1c447181ac6f36933fcfca4159052f58bdb2b
JMC1027 <87082922+JMC1027@users.noreply.github.com>
2021-08-05 15:24:39 -0400

    project_comparison

---
+++ Basic_Javascript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,14 @@
+document.write(typeof "Snowboard" + " ");
+document.write(2 + 2 + " ");
+
+document.write(10 > 5 && 12 > 6);
+document.write(15 > 10 || 10 > 15);
+document.write(12 == 12);
+x = 20;
+z = 20;
+document.write(x === z);
+document.write(25 > 24);
+document.write(30 < 31);
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(30 > 25);
+}
